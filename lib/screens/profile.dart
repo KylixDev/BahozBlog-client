3b5977442616c86366f64f366e5a104fb1dbe7da
Parsa Nik <94043228+Parsa-Nik@users.noreply.github.com>
2022-12-21 01:02:13 +0330

    Update profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,298 +1,166 @@
-// ignore_for_file: non_constant_identifier_names
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/svg.dart';
-import '../models/blog_model.dart';
+import '../models/profile_model.dart';
+import '../widgets/w_header.dart';
 
-class MyListTile extends StatelessWidget {
-  const MyListTile({super.key});
+//ParsaNik
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const RecommendedPosts(),
-    );
-  }
+  State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ArticleDescription extends StatelessWidget {
-  _ArticleDescription({required this.BlogDatas});
-
-  final BlogModel BlogDatas;
-
+class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                BlogDatas.title,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                decoration: BoxDecoration(
-                  color: Color(0xFF2E8EFF).withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
+    bool hasNotification = true;
+    final myController = TextEditingController();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            getHomeHeader(context, hasNotification, myController),
+            //
+            //
+            //
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
                   children: [
-                    Icon(Icons.tag, size: 12),
-                    Text(
-                      BlogDatas.category,
-                      style: const TextStyle(
-                        fontSize: 12.0,
-                        color: Colors.black87,
-                      ),
+                    Column(
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/94043228?v=4'),
+                            radius: 30,
+                          ),
+                          title: Padding(
+                            padding: const EdgeInsets.only(bottom: 6.0),
+                            child: Text('پارسا نیک سیرت'),
+                          ),
+                          subtitle: Text('فلاتر دولوپر'),
+                        ),
+                        //
+                        //
+                        SizedBox(
+                          height: 12,
+                        ),
+                        //
+                        //
+                        //
+                        Container(
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 40,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  '۱۲ دنبال شونده',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 60,
+                                child: VerticalDivider(
+                                  width: 30,
+                                  thickness: 2,
+                                  indent: 20,
+                                  endIndent: 20,
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  '۱۲ دنبال کننده',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 60,
+                                child: VerticalDivider(
+                                  width: 30,
+                                  thickness: 2,
+                                  indent: 20,
+                                  endIndent: 20,
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  '۱۲ مقالات',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24,
+                        ),
+                        //
+                        //
+                        TitleOfSections(
+                          text: 'پیش نویس',
+                          icon: 'assets/icons/icn_main/slidervertical.svg',
+                        ),
+                        SizedBox(height: 18),
+
+                        /* get Recommended Blog Posts */
+                        Draft(),
+                        TitleOfSections(
+                          text: 'منتشر شده',
+                          icon: 'assets/icons/icn_main/slidervertical.svg',
+                        ),
+                        SizedBox(height: 18),
+
+                        /* get Recommended Blog Posts */
+                        RecommendedPosts(),
+                      ],
                     ),
                   ],
                 ),
               ),
-              SizedBox(height: 2),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+            //
+            //
+            //
+          ],
+        ),
+        drawer: Profile_drawer(),
+      ),
+    );
+  }
+
+  // Return Header/AppBar Widget
+  Widget getHomeHeader(context, hasNotification, myController) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    '${BlogDatas.publishDate}  •  ${BlogDatas.readDuration}',
-                    style: const TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.black54,
-                    ),
-                  ),
-                  Spacer(),
-                  SvgPicture.asset('assets/icons/icn_home/more.svg')
+                  SvgPicture.asset('assets/images/img_profile/logo.svg'),
+                  GestureDetector(
+                    onTap: () {},
+                    child:
+                        SvgPicture.asset('assets/icons/icn_profile/menu.svg'),
+                  )
                 ],
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class CustomListItem extends StatelessWidget {
-  const CustomListItem({
-    super.key,
-    required this.thumbnail,
-    required this.title,
-    required this.category,
-    required this.publishDate,
-    required this.lastEditDate,
-    required this.readDuration,
-    required this.isSaved,
-    required this.isDownloaded,
-  });
-
-  final String thumbnail;
-  final String title;
-  final String category;
-  final String publishDate;
-  final String lastEditDate;
-  final String readDuration;
-  final bool isSaved;
-  final bool isDownloaded;
-
-  get BlogDatas => BlogModel(
-        title: title,
-        thumbnail: thumbnail,
-        category: category,
-        publishDate: publishDate,
-        lastEditDate: lastEditDate,
-        readDuration: readDuration,
-        isSaved: isSaved,
-        isDownloaded: isDownloaded,
-      );
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          // height: MediaQuery.of(context).size.height / 8.5,
-          height: 100,
-
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: 4 / 3,
-                child: ClipRRect(
-                  child: Container(
-                    color: Color(0xFFEBEBEB),
-                    child: Image.network(
-                      thumbnail,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Center(
-                          child: Text(
-                            'بدون اینترنت :/',
-                            style: TextStyle(
-                              fontSize: 12,
-                            ),
-                          ),
-                        );
-                      },
-                      loadingBuilder: (BuildContext context, Widget child,
-                          ImageChunkEvent? loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return Center(
-                          child: SpinKitThreeBounce(
-                            size: 20.0,
-                            color: Color(0xFF2E8EFF),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 5.0, 12.0, 5.0),
-                  child: _ArticleDescription(
-                    BlogDatas: BlogDatas,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: Divider(thickness: 1.5, color: Color(0xFFEFEFEF)),
-        )
-      ],
-    );
-  }
-}
-
-class RecommendedPosts extends StatelessWidget {
-  const RecommendedPosts({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      children: <Widget>[
-        CustomListItem(
-          thumbnail:
-              'https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=80',
-          title: 'روند پیشرفت تکنولوژی در سال ۲۰۲۲',
-          category: 'تکنولوژی',
-          publishDate: '۱۲/۴/۲۲',
-          readDuration: 'خواندن در ۱۵ دقیقه',
-          lastEditDate: '',
-          isSaved: true,
-          isDownloaded: false,
-        ),
-        CustomListItem(
-          thumbnail:
-              'https://images.unsplash.com/photo-1581089781785-603411fa81e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=80',
-          title: 'تجهیزات جدید شگفت انگیز برای AR و دوستداران واقعیت مجازی',
-          category: 'تکنولوژی',
-          publishDate: '۱۲/۴/۲۲',
-          readDuration: 'خواندن در ۷ دقیقه',
-          lastEditDate: '',
-          isSaved: true,
-          isDownloaded: false,
-        ),
-        CustomListItem(
-          thumbnail:
-              'https://images.unsplash.com/photo-1580894742597-87bc8789db3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=80',
-          title: 'شیرجه زدن به دنیای جدید هوشمند...',
-          category: 'برنامه نویسی',
-          publishDate: '۱۲/۴/۲۲',
-          readDuration: 'خواندن در ۷ دقیقه',
-          lastEditDate: '',
-          isSaved: false,
-          isDownloaded: false,
-        ),
-        CustomListItem(
-          thumbnail:
-              'https://images.unsplash.com/photo-1504384764586-bb4cdc1707b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=80',
-          title: 'معرفی سیستم دیزاین جدید گوگل به اسم متریال یو',
-          category: 'دیزاین',
-          publishDate: '۱۲/۴/۲۲',
-          readDuration: 'خواندن در ۷ دقیقه',
-          lastEditDate: '',
-          isSaved: false,
-          isDownloaded: false,
-        ),
-        CustomListItem(
-          thumbnail:
-              'https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=80',
-          title: '۱۰ ابزار خفن برای هوش مصنوعی',
-          category: 'هوش مصنوعی',
-          publishDate: '۱۲/۴/۲۲',
-          readDuration: 'خواندن در ۷ دقیقه',
-          lastEditDate: '',
-          isSaved: false,
-          isDownloaded: false,
-        ),
-      ],
-    );
-  }
-}
-
-// برای پیش نویس پروفایل
-class Draft extends StatelessWidget {
-  const Draft({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      children: <Widget>[
-        CustomListItem(
-          thumbnail:
-              'https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=80',
-          title: '۱۰ ابزار خفن برای هوش مصنوعی',
-          category: 'هوش مصنوعی',
-          publishDate: '۱۲/۴/۲۲',
-          readDuration: 'خواندن در ۷ دقیقه',
-          lastEditDate: '',
-          isSaved: false,
-          isDownloaded: false,
-        ),
-      ],
-    );
-  }
-}
-
-// Profile Drawer
-
-class Profile_drawer extends StatelessWidget {
-  const Profile_drawer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Colors.amber,
+              )),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 28,
+          )
+        ],
+      ),
     );
   }
 }
